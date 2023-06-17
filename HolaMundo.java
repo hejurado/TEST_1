104a427228d5acc63e92a7d55d4f4a1a8b2dd4cd
hector jurado <hejurado@gmail.com>
2023-06-16 21:37:42 -0500

    conversion a proyecto maven

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,9 +1,11 @@
-public class HolaMundo{
+public class HolaMundo {
 
-public static void main(String[]args){
-System.out.println("Hola mundo");
-System.out.println("ADSO");
-System.out.println ("BIENVENIDOS");
-}
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("ADSO");
+		System.out.println("BIENVENIDOS");
+		System.out.println("BIENVENIDOS");
+		System.out.println("BIENVENIDOS OTRA VEZ ");
+	}
 
 }
